748ed2647c418dafb64794d1248bc0e668d4f7de
Mohigul <mohigulsharipova44@gmail.com>
2024-12-02 17:01:50 +0500

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import pymunk
+import pymunk.pygame_util
+import math
+
+# common
+TITLE = "Pool Game"
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 678
+BOTTOM_PANEL = 50
+BACKGROUND_COLOR = (50, 50, 50)
+TEXT_COLOR = (255, 255, 255)
