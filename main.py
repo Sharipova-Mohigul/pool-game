92670d2aa28ff3080dea014d25195c999bf357a5
Mohigul <mohigulsharipova44@gmail.com>
2024-12-03 10:54:20 +0500

    load images

--- main.py
+++ main.py
@@ -73,3 +73,16 @@ cue_ball_potted = False
 taking_shot = True
 powering_up = False
 potted_balls = []
+
+# load images
+cue_image = pygame.image.load("assets/images/cue.png").convert_alpha()
+table_image = pygame.image.load("assets/images/table.png").convert_alpha()
+ball_images = []
+for i in range(1, MAX_BALL):
+    ball_image = pygame.image.load(f"assets/images/ball_{i}.png").convert_alpha()
+    ball_images.append(ball_image)
+
+
+# function for outputting text onto the screen
+def draw_text(text, font, text_col, x, y):
+    screen.blit(font.render(text, True, text_col), (x, y))
