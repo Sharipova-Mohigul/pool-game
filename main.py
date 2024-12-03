0f970e2b1ba5c18e1238c5d5fb80ea5c98c0190f
Orzugul <orzugulnorova5@gmail.com>
2024-12-03 12:14:25 +0500

    class

--- main.py
+++ main.py
@@ -133,3 +133,27 @@ def create_cushion(poly_dims):
 
 for cushion in CUSHIONS:
     create_cushion(cushion)
+    # create pool cue
+class Cue:
+    def __init__(self, pos):
+        self.original_image = cue_image
+        self.angle = 0
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self, angle):
+        self.angle = angle
+
+    def draw(self, surface):
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        surface.blit(
+            self.image,
+            (
+                self.rect.centerx - self.image.get_width() / 2,
+                self.rect.centery - self.image.get_height() / 2,
+            ),
+        )
+
+
+cue = Cue(balls[-1].body.position)
